0fc11d5aaf6766172b988fb192ac59f4fcae5038
Krystian <krystiankrz@gmail.com>
2015-05-13 19:04:15 +0200

    Create editor.php

---
+++ anchor/views/partials/editor.php
@@ -0,0 +1,8 @@
+<nav>
+	<a class="icon bold" href="#bold">Pogrubienie</a>
+	<a class="icon italic" href="#italic">Kursywa</a>
+	<a class="icon list" href="#list">Lista</a>
+	<a class="icon quote" href="#quote">Cytat</a>
+	<a class="icon code" href="#code">Kod</a>
+	<a class="icon link" href="#link">Link</a>
+</nav>

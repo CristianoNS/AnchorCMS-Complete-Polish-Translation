6d9f6f6d03438dcedcd9dfe69ed39fb8e95e92df
Krystian <krystiankrz@gmail.com>
2015-05-13 19:03:09 +0200

    Create index.php

---
+++ anchor/views/extend/plugins/index.php
@@ -0,0 +1,15 @@
+<?php echo $header; ?>
+
+<hgroup class="wrap">
+	<h1>Pluginy</h1>
+</hgroup>
+
+<section class="wrap">
+	<?php echo $messages; ?>
+
+	<p class="empty">
+		<span class="icon"></span> Wkrótce
+	</p>
+</section>
+
+<?php echo $footer; ?>

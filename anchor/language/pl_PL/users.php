2102038e178c284ce7e34deb53920e7448bb9493
Krystian <krystiankrz@gmail.com>
2015-05-13 17:51:32 +0200

    Create users.php

---
+++ anchor/language/pl_PL/users.php
@@ -0,0 +1,67 @@
+<?php
+
+return array(
+
+	'users' => 'Użytkownicy',
+
+	'create_user' => 'Utwórz użytkownika',
+	'add_user' => 'Dodaj użytkownika',
+	'editing_user' => 'Edycja użytkownika: %s&rsquo;s',
+	'remembered' => 'Zapamiętaj mnie',
+	'forgotten_password' => 'Zapomniałeś hasła?',
+
+	// roles
+	'administrator' => 'Admin',
+	'administrator_explain' => 'Administrator witryny',
+
+	'editor' => 'Redaktor bloga',
+	'editor_explain' => '',
+
+	'user' => 'Użytkownik',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => 'Imię',
+	'real_name_explain' => 'Podaj swoje imię',
+
+	'bio' => 'O mnie',
+	'bio_explain' => 'Napisz coś o sobie',
+
+	'status' => 'Status',
+	'status_explain' => 'Określ status użytkownika',
+
+	'role' => 'Ranga',
+	'role_explain' => 'Określ rangę użytkownika',
+
+	'username' => 'Login',
+	'username_explain' => 'Login użytkownika',
+	'username_missing' => 'Nazwa użytkownika musi mieć minimum %s znaków',
+
+	'password' => 'Hasło',
+	'password_explain' => 'Hasło użytkownika',
+	'password_too_short' => 'Hasło użytkownika musi mieć minimum %s znaków',
+
+	'new_password' => 'Nowe hasło',
+
+	'email' => 'Email',
+	'email_explain' => 'Email użytkownika',
+	'email_missing' => 'Podaj prawidłowy adres email',
+	'email_not_found' => 'Profil nie został znaleziony',
+
+	// messages
+	'updated' => 'Profil został zaktualizowany',
+	'created' => 'Profil został utworzony',
+	'deleted' => 'Profil został usunięty',
+	'delete_error' => 'Nie można usunąć tego profilu',
+	'login_error' => 'Nazwa użytkownika lub hasło są nieprawidłowe',
+	'logout_notice' => 'Zostałeś wylogowany',
+	'recovery_sent' => 'Wysłaliśmy ci email w celu potwierdzenia hasła',
+	'recovery_expired' => 'Token odzyskania hasła wygasł, spróbuj ponownie',
+	'password_reset' => 'Twoje hasło zostało utworzone',
+
+	// password recovery email
+	'recovery_subject' => 'Przypomnienie hasła',
+	'recovery_message' => 'Poprosiłeś o zmianę hasła' .
+		'Aby kontynuować wejdź pod.' . PHP_EOL . '%s',
+
+);

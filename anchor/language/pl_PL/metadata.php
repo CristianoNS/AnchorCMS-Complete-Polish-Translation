61b293065c024b2f6c29d4bf21a3ec11f299f444
Krystian <krystiankrz@gmail.com>
2015-05-13 17:18:54 +0200

    Create metadata.php

---
+++ anchor/language/pl_PL/metadata.php
@@ -0,0 +1,44 @@
+<?php
+
+return array(
+
+	'metadata' => 'Ustawienia metadanych',
+	'metadata_desc' => 'Zarządzanie danymi strony',
+
+	'comment_settings' => 'Komentarze',
+	'theme_settings' => 'Wygląd',
+
+	// form fields
+	'sitename' => 'Nazwa strony',
+	'sitename_explain' => 'Nadaj nazwę dla swojej strony',
+	'sitename_missing' => 'Musisz podać nazwę strony',
+
+	'sitedescription' => 'Opis strony',
+	'sitedescription_explain' => 'Nadaj opis swojej strony',
+	'sitedescription_missing' => 'Musisz podać opis strony',
+
+	'homepage' => 'Główna strona',
+	'homepage_explain' => 'Wybierz stronę główną',
+
+	'postspage' => 'Strona z postami',
+	'postspage_explain' => 'Wybierz stronę z postami',
+
+	'posts_per_page' => 'Postów na stronę',
+	'posts_per_page_explain' => 'Zaznacz, ile postów ma być wyświetlanych',
+
+	'auto_publish_comments' => 'Automatyczne zatwierdzanie komentarzy',
+	'auto_publish_comments_explain' => 'Włącz lub wyłącz automatyczne zatwierdzanie komentarzy',
+
+	'comment_notifications' => 'Powiadamiaj o nowym komentarzu',
+	'comment_notifications_explain' => 'Włącz lub wyłącz powiadomienia o nowych komentarzach',
+
+	'comment_moderation_keys' => 'Zabronione słowa',
+	'comment_moderation_keys_explain' => 'Podaj słowa po przecinku, które mają być uznane automatycznie za spam',
+
+	'current_theme' => 'Szablon strony',
+	'current_theme_explain' => 'Wybierz szablon dla swojej strony',
+
+	// messages
+	'updated' => 'Ustawienia zapisane',
+
+);

beb0d9089c3c3305bf3f51e5f0579f85378d43da
Krystian <krystiankrz@gmail.com>
2015-05-13 16:16:26 +0200

    Create categories.php

---
+++ anchor/language/pl_PL/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'category' => 'Kategoria',
+	'categories' => 'Kategorie',
+
+	'create_category' => 'Utwórz nową kategorię',
+	'edit_category' => 'Edytujesz &ldquo;%s&rdquo;',
+
+	// form fields
+	'title' => 'Tytuł',
+	'title_explain' => 'Tytuł kategorii.',
+	'title_missing' => 'Podaj tytuł kategorii.',
+
+	'slug' => 'URL',
+	'slug_explain' => 'Link do kategorii.',
+
+	'description' => 'Opis',
+	'description_explain' => 'Opis Twojej kategorii.',
+
+	// messages
+	'created' => 'Kategoria została utworzona.',
+	'updated' => 'Kategoria została zaktualizowana.',
+	'deleted' => 'Kategoria została usunięta.',
+	'delete_error' => 'Musisz mieć co najmniej jedną kategorię.',
+
+);

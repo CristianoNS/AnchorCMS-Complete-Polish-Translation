4c297b6e576c9a8426e74febe0a601b648867a5a
Krystian <krystiankrz@gmail.com>
2015-05-13 15:45:36 +0200

    Create complete.php

---
+++ install/views/complete.php
@@ -0,0 +1,19 @@
+<?php echo $header; ?>
+
+<section class="content small">
+	<h1>Instalacja zakończona!</h1>
+
+	<?php if($htaccess): ?>
+	<p class="code">Nie mogliśmy stworzyć pliku <code>htaccess</code>. Skopiuj poniższą treść i utwórz plik .htaccess w głównym folderze Anchor.
+	<textarea id="htaccess"><?php echo $htaccess; ?></textarea></p>
+
+	<script>document.getElementById('htaccess').select();</script>
+	<?php endif; ?>
+
+	<section class="options">
+		<a href="<?php echo $admin_uri; ?>" class="button">Odwiedź panel administratora</a>
+		<a href="<?php echo $site_uri; ?>" class="right">Odwiedź twoją stronę</a>
+	</section>
+</section>
+
+<?php echo $footer; ?>

617b496d1fb33ec853e3d9805d78129748bfd15a
Krystian <krystiankrz@gmail.com>
2015-05-13 17:26:53 +0200

    Create pages.php

---
+++ anchor/language/pl_PL/pages.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'pages' => 'Strony',
+
+	'create_page' => 'Utwórz nową stronę',
+	'nopages_desc' => 'Nie posiadasz żadnych stron',
+	'redirect' => 'Przekierowanie',
+
+	// form fields
+	'redirect_url' => 'Adres przekierowania',
+	'redirect_missing' => 'Podaj prawidłowy adres strony',
+
+	'title' => 'Tytuł strony',
+	'title_explain' => 'Podaj tytuł strony',
+	'title_missing' => 'Podaj tytuł strony',
+
+	'content' => 'Treść',
+	'content_explain' => 'Zacznij pisać',
+
+	'show_in_menu' => 'Pokaż w menu',
+	'show_in_menu_explain' => 'Włącz lub wyłącz pokazywanie strony w menu',
+
+	'name' => 'Nazwa',
+	'name_explain' => 'Wpisz nazwę strony',
+
+	'slug' => 'URL',
+	'slug_explain' => 'Wpisz adres URL',
+	'slug_missing' => 'Podaj prawidłowy adres URL strony',
+	'slug_duplicate' => 'Taki adres już istnieje',
+	'slug_invalid' => 'Adres URL musi zawierać litery',
+
+	'status' => 'Status',
+	'status_explain' => 'Określ status strony',
+
+	'parent' => 'Rodzic',
+	'parent_explain' => 'Określ rodzica strony',
+
+	// messages
+	'updated' => 'Strona została zaktualizowana',
+	'created' => 'Strona została utworzona',
+	'deleted' => 'Strona została usunięta'
+
+);

9e6b3a7ad19d667f0c02338173089687367e66f0
Krystian <krystiankrz@gmail.com>
2015-05-13 17:12:56 +0200

    Create posts.php

---
+++ anchor/language/pl_PL/posts.php
@@ -0,0 +1,47 @@
+<?php
+
+return array(
+
+	'posts' => 'Posty',
+
+	'create_post' => 'Utwórz nowy post',
+	'noposts_desc' => 'Nie posiadasz żadnych postów',
+
+	// form fields
+	'title' => 'Tytuł posta',
+	'title_explain' => 'Podaj tytuł posta',
+	'title_missing' => 'Podaj tytuł posta',
+
+	'content' => 'Treść posta',
+	'content_explain' => 'Zacznij pisać',
+
+	'slug' => 'URL',
+	'slug_explain' => 'Adres URL Twojego posta',
+	'slug_missing' => 'Podaj prawidłowy adres URL strony',
+	'slug_duplicate' => 'Taki adres już istnieje',
+	'slug_invalid' => 'Adres URL musi zawierać litery',
+
+	'description' => 'Opis',
+	'description_explain' => 'Podaj krótki opis posta',
+
+	'status' => 'Status',
+	'status_explain' => 'Nadaj status posta',
+
+	'category' => 'Kategoria',
+	'category_explain' => 'Wybierz kategorię, w której ma znaleźć się post',
+
+	'allow_comments' => 'Komentowanie',
+	'allow_comments_explain' => 'Włącz lub wyłącz komentowanie',
+
+	'custom_css' => 'Dodatkowy CSS',
+	'custom_css_explain' => '',
+
+	'custom_js' => 'Dodatkowy JS',
+	'custom_js_explain' => '',
+
+	// messages
+	'updated' => 'Post został zaktualizowany',
+	'created' => 'Post został utworzony',
+	'deleted' => 'Post został usunięty'
+
+);

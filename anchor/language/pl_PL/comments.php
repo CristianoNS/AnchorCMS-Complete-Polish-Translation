2d49cea8f07d26b0e3cedaf8651112aaced236f7
Krystian <krystiankrz@gmail.com>
2015-05-13 18:58:19 +0200

    Create comments.php

---
+++ anchor/language/pl_PL/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+	'comments' => 'Komentarze',
+	'nocomments_desc' => 'Brak komentarzy',
+	'editing_comment' => 'Edycja komentarza',
+	'view_comment' => 'Zobacz komentarz',
+
+	// form fields
+	'name' => 'Imię',
+	'name_explain' => 'Imię autora',
+	'name_missing' => 'Proszę podać imię',
+
+	'email' => 'Email',
+	'email_explain' => 'Adres email',
+	'email_missing' => 'Musisz podać poprawny adres email', // frontend message (appears on your site!)
+
+	'text' => 'Komentarz',
+	'text_explain' => '',
+	'text_missing' => 'Proszę podać treść komentarza', // frontend message (appears on your site!)
+
+	'status' => 'Status',
+	'status_explain' => 'Określ status komentarza',
+
+	// messages
+	'created' => 'Komentarz został dodany', // frontend message (appears on your site!)
+	'updated' => 'Kmentarz został zaktualizowany',
+	'deleted' => 'Komentarz został usunięty',
+
+	// email notification
+	'notify_subject' => 'Nowy komentarz został dodany',
+	'nofity_heading' => 'Nowy komentarz został dodany na Twoją stronę'
+
+);

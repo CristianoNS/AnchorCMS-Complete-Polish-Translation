6df34c06a49f583b47d6574ead13fe82be5d2ccc
Krystian <krystiankrz@gmail.com>
2015-05-13 17:33:02 +0200

    Create global.php

---
+++ anchor/language/pl_PL/global.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Zapisz',
+	'delete' => 'Usuń',
+	'update' => 'Zaktualizuj',
+	'edit' => 'Edytuj',
+	'editing' => 'Edytowane',
+	'create' => 'Utwórz',
+	'created' => 'Utworzone',
+	'submit' => 'Zgłoś',
+	'close' => 'Zamknij',
+	'status' => 'Status',
+	'manage' => 'Zarządzaj',
+	'reset' => 'Reset',
+	'all' => 'Wszystkie',
+
+	// pagination
+	'next' => 'Następny',
+	'previous' => 'Poprzedni',
+	'first' => 'Pierwszy',
+	'last' => 'Ostatni',
+
+	// statuses
+	'draft' => 'Szkice',
+	'archived' => 'Zarchiwizowane',
+	'published' => 'Opublikowane',
+	'pending' => 'Oczekujące',
+	'approved' => 'Zatwierdzone',
+	'spam' => 'Spam',
+
+	'inactive' => 'Niekatywny',
+	'active' => 'Aktywny',
+
+	// roles
+	'administrator' => 'Admin',
+	'editor' => 'Edytor',
+	'user' => 'Użytkownik',
+
+	'log_in' => 'Zaloguj się',
+	'login' => 'Zaloguj',
+	'log_out' => 'Wyloguj się',
+	'logout' => 'Wyloguj',
+
+	// pharses
+	'visit_your_site' => 'Zobacz stronę',
+	'powered_by_anchor' => 'Napędzani przez Anchor %s',
+	'make_blogging_beautiful' => 'Blogowanie jest piękne.',
+
+	// intro
+	'welcome_to_anchor' => 'Witaj w Anchor',
+	'welcome_to_anchor_lets_go' => 'Witaj w Anchor. Naprzód!.',
+	'run_the_installer' => 'Rozpocznij instalację',
+
+	// upgrade
+	'upgrade' => 'Aktualizacja',
+	'good_news' => 'Dobra wiadomość!',
+	'new_version_available' => 'Aktualizacja jest dostępna',
+	'download_now' => 'Pobierz teraz',
+	'upgrade_later' => 'Pobierz później',
+
+	// debug profiler
+	'profile' => 'Profil',
+	'profile_memory_usage' => 'Używana pamięć',
+
+	// messages
+	'confirm_delete' => 'Jesteś pewny, że chcesz usunąć? Nie można tego cofnąć!'
+
+);

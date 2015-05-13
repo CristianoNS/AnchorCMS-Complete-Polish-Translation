ce10fce3fb58abdcb072c4bf32050f519f75447b
Krystian <krystiankrz@gmail.com>
2015-05-13 19:06:27 +0200

    Create 404.php

---
+++ themes/default/404.php
@@ -0,0 +1,9 @@
+<?php theme_include('header'); ?>
+
+	<section class="content wrap">
+		<h1>Nie znaleziono strony</h1>
+
+		<p>Niestety, strony <code>/<?php echo current_url(); ?></code> nie znaleziono. Spróbuj ponownie lub przejdź na <a href="<?php echo base_url(); ?>">stronę główną</a> i użyj <a href="#search">wyszukiwarki</a>, albo idź płakać w kącie (aczkolwiek nie polecamy tego ostatniego).</p>
+	</section>
+
+<?php theme_include('footer'); ?>

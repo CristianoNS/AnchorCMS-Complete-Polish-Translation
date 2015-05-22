7258228c155cb992ba64a9f22e8f75428bda55d3
Krystian <krystiankrz@gmail.com>
2015-05-22 21:55:03 +0200

    Update 404.php

--- themes/default/404.php
+++ themes/default/404.php
@@ -1,9 +1,8 @@
 <?php theme_include('header'); ?>
 
-	<section class="content wrap">
-		<h1>Nie znaleziono strony</h1>
-
-		<p>Niestety, strona <code>/<?php echo current_url(); ?></code> nie została znaleziona. Spróbuj ponownie lub przejdź na <a href="<?php echo base_url(); ?>">stronę główną</a> i użyj <a href="#search">wyszukiwarki</a>, albo idź płakać w kącie (aczkolwiek nie polecamy tego ostatniego).</p>
-	</section>
+<section class="content wrap">
+	<h1>Nie znaleziono strony</h1>
+	<p>Szukana strona nie została znaleziona. Spróbuj ponownie lub przejdź na <a href="<?php echo base_url(); ?>">stronę główną</a> i użyj <a href="#search">wyszukiwarki</a>.</p>
+</section>
 
 <?php theme_include('footer'); ?>

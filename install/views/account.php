b9d9cf64ce9662878e318ae1c73185d712ed0319
Krystian <krystiankrz@gmail.com>
2015-05-22 22:22:23 +0200

    Update account.php

--- install/views/account.php
+++ install/views/account.php
@@ -14,20 +14,20 @@
 		<fieldset>
 			<p>
 				<label for="username">Nazwa użytkownika</label>
-				<i>Użyj tego, aby się zalogować.</i>
+				<i>Wybierz swoją nazwę użytkownika</i>
 				<input tabindex="1" id="username" name="username" value="<?php echo Input::previous('username', 'admin'); ?>">
 			</p>
 
 			<p>
 				<label for="email">Adres email</label>
-				<i>Potrzebne, jeśli nie możesz się zalogować.</i>
+				<i>Podaj swój adres email</i>
 
 				<input tabindex="2" id="email" type="email" name="email" value="<?php echo Input::previous('email'); ?>">
 			</p>
 
 			<p>
 				<label>Hasło</label>
-				<i>Pamiętaj, aby stworzyć trudne hasło.</i>
+				<i>Wpisz swoje silne hasło</i>
 				<input tabindex="3" name="password" type="password" value="<?php echo Input::previous('password'); ?>">
 			</p>
 		</fieldset>

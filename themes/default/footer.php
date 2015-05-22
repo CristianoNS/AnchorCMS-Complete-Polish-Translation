588b8d608692c0dd7034984259a47314df189c84
Krystian <krystiankrz@gmail.com>
2015-05-22 21:52:36 +0200

    Update footer.php

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -1,20 +1,14 @@
 		<div class="wrap">
-	            <footer id="bottom">
-	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. Wszelkie prawa zastrzeżone.</small>
-
-	                <ul role="navigation">
-	                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
-	                    <?php if(twitter_account()): ?>
-	                    <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
-	                    <?php endif; ?>
-
-	                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Panel admina</a></li>
-
-	                    <li><a href="/" title="Return to my website.">Strona główna</a></li>
-	                </ul>
-	            </footer>
-
-	        </div>
-        </div>
-    </body>
+			<footer id="bottom">
+				<small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>.</small>
+				<ul role="navigation">
+					<li><a href="<?php echo rss_url(); ?>" title="Kanał RSS">RSS</a></li>
+					<li><a href="http://nostatic.pl" title="Nostatic.pl - Polskie wsparcie AnchorCMS">Polskie wsparcie</a></li>
+					<li><a href="<?php echo base_url('admin'); ?>" title="Przejdź do panelu administratora">Panel admina</a></li>
+					<li><a href="/" title="Wróć na stronę główną">Strona główna</a></li>
+				</ul>
+			</footer>
+		</div>
+	</div>
+</body>
 </html>

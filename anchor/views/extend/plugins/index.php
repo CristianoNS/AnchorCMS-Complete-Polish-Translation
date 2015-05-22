e1a9f70fa805b188098ac208d2dce845ca2422d2
Krystian <krystiankrz@gmail.com>
2015-05-22 22:56:05 +0200

    Update index.php

--- anchor/views/extend/plugins/index.php
+++ anchor/views/extend/plugins/index.php
@@ -3,13 +3,9 @@
 <hgroup class="wrap">
 	<h1>Pluginy</h1>
 </hgroup>
-
 <section class="wrap">
 	<?php echo $messages; ?>
-
-	<p class="empty">
-		<span class="icon"></span> Wkrótce
-	</p>
+	<p class="empty"><span class="icon"></span>Wkrótce</p>
 </section>
 
 <?php echo $footer; ?>

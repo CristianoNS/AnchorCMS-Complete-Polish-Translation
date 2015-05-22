9a34728f03fb043e971ae345c082dddadd0ea269
Krystian <krystiankrz@gmail.com>
2015-05-22 22:28:01 +0200

    Update halt.php

--- install/views/halt.php
+++ install/views/halt.php
@@ -15,7 +15,7 @@
 		<?php endif; ?>
 
 		<p class="options">
-			<a class="btn" href="<?php echo uri_to('start'); ?>">Spróbuj ponownie.</a>
+			<a class="btn" href="<?php echo uri_to('start'); ?>">Spróbuj ponownie</a>
 		</p>
 	</article>
 </section>

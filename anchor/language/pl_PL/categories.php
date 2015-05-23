775df34435290c8f1f049603d9dd0660692a69dd
Krystian <krystiankrz@gmail.com>
2015-05-23 10:24:06 +0200

    Update categories.php

--- anchor/language/pl_PL/categories.php
+++ anchor/language/pl_PL/categories.php
@@ -6,23 +6,23 @@ return array(
 	'categories' => 'Kategorie',
 
 	'create_category' => 'Utwórz nową kategorię',
-	'edit_category' => 'Edytujesz &ldquo;%s&rdquo;',
+	'edit_category' => 'Edytujesz: &ldquo;%s&rdquo;',
 
 	// form fields
 	'title' => 'Tytuł',
-	'title_explain' => 'Tytuł kategorii.',
-	'title_missing' => 'Podaj tytuł kategorii.',
+	'title_explain' => 'Tytuł kategorii',
+	'title_missing' => 'Podaj tytuł kategorii',
 
 	'slug' => 'URL',
-	'slug_explain' => 'Link do kategorii.',
+	'slug_explain' => 'Link do kategorii',
 
 	'description' => 'Opis',
-	'description_explain' => 'Opis Twojej kategorii.',
+	'description_explain' => 'Opis Twojej kategorii',
 
 	// messages
-	'created' => 'Kategoria została utworzona.',
-	'updated' => 'Kategoria została zaktualizowana.',
-	'deleted' => 'Kategoria została usunięta.',
-	'delete_error' => 'Musisz mieć co najmniej jedną kategorię.',
+	'created' => 'Kategoria została utworzona',
+	'updated' => 'Kategoria została zaktualizowana',
+	'deleted' => 'Kategoria została usunięta',
+	'delete_error' => 'Musisz mieć co najmniej jedną kategorię',
 
 );
